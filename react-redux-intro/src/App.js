cc4649a8a9688f7573cb6a124e15cf9e710b9014
sefadogan <sefadogan7@gmail.com>
2019-08-14 23:32:36 +0300

    Committed all project files.

---
+++ react-redux-intro/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+import DecreaseCounter from './components/DecreaseCounter';
+import IncreaseByTwoCounter from './components/IncreaseByTwoCounter';
+import IncreaseCounter from './components/IncreaseCounter';
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <IncreaseCounter />
+      <DecreaseCounter />
+      <IncreaseByTwoCounter />
+    </div>
+  );
+}
+
+export default App;
